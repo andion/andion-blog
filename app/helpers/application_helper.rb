d77ff7c8f48c4079b63163862dc54618d733a5bf
Lucas Andion <andion@gmail.com>
2009-01-19 00:59:17 +0100

    Styles enhacement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,12 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  #
+  # Returns a back to blog index page if this is not the index page
+  #
+  def back_to_index_link
+    unless current_page?(:controller => 'posts', :action => 'index') or current_page?('/')
+	    link_to '&laquo; Back to blog index', posts_path, :class=>"back_header"
+	  end
+  end
 end

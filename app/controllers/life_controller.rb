ac1b8aa9f8a7ec275262473dbfc002a354aedb08
Lucas Andion <lucas.andion@trabesoluciones.com>
2009-07-06 15:15:09 +0200

    Quick fix to twitter problem to make the blog work

--- app/controllers/life_controller.rb
+++ app/controllers/life_controller.rb
@@ -2,23 +2,24 @@ class LifeController < ApplicationController
 
   require 'rubygems'
   require 'twitter'
-    
+
   layout 'life'
 
   def show
     twitts = []
     my_twitts = []
 
-    Twitter::Search.new.from('bugyou').each do |t|
-      my_twitts << t
-    end
+#        Twitter::Search.new.from('bugyou').each do |t|
+#          my_twitts << t
+#        end
 
-    Twitter::Search.new('bugyou').each do |t|
-      twitts << t unless my_twitts.include? t
-    end
-        
-    @twitts =  twitts [0..2]
-    @my_twitts = my_twitts[0..2]
+#       Twitter::Search.new('bugyou').each do |t|
+#            twitts << t unless my_twitts.include? t
+#        end
+
+        @twitts =  twitts #[0..2]
+        @my_twitts = my_twitts #[0..2]
+
   end
 
   #
@@ -31,3 +32,4 @@ class LifeController < ApplicationController
   end
 
 end
+

13858c399230df6b5bcaef5fb98ccb92e316f026
andion <andion@gmail.com>
2009-09-03 23:09:59 +0200

    Now twits without ooemiso, modified introduction a little bit

--- app/controllers/life_controller.rb
+++ app/controllers/life_controller.rb
@@ -13,8 +13,17 @@ class LifeController < ApplicationController
       my_twitts << t
     end
 
+<<<<<<< HEAD:app/controllers/life_controller.rb
+        Twitter::Search.new('bugyou').each do |t|
+            twitts << t unless my_twitts.include? t || t.from_user == 'ooemiso'
+        end
+        
+        @twitts =  twitts [0..2]
+        @my_twitts = my_twitts[0..2]
+=======
     Twitter::Search.new('bugyou').each do |t|
       twitts << t unless my_twitts.include? t
+>>>>>>> 10d9b61e816253fc332cd763a996363dee0bb1ef:app/controllers/life_controller.rb
     end
         
     @twitts =  twitts [0..2]
